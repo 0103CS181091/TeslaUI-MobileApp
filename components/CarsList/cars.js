82e695ed2357a3ed0f9ee72f564a65a3ec0f78ec
Nikhil Patidar <nikhilpatidar0@gmail.com>
2021-07-15 22:36:39 +0530

    Update cars.js

--- components/CarsList/cars.js
+++ components/CarsList/cars.js
@@ -1,20 +1,35 @@
-export default [{
+export default [
+  {
   name: 'Model S',
   tagline: 'Starting at $69,420',
   image: require('../../assets/images/ModelS.jpeg'),
-}, {
+  },
+  {
   name: 'Model 3',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/Model3.jpeg'),
-}, {
+  },
+  {
   name: 'Model X',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelX.jpeg'),
-}, {
+  },
+  {
   name: 'Model Y',
   tagline: 'Order Online for',
   taglineCTA: 'Touchless Delivery',
   image: require('../../assets/images/ModelY.jpeg'),
-}];
+  },
+  {
+  name: 'Solar Panel',
+  tagline: 'Lowest Cost Solar Panels in America',
+  image: require('../../assets/images/SolarPanels.jpeg'),
+  },
+  {
+  name: 'Solar Roofs',
+  tagline: 'Produce Clean Energy From Your Roof',
+  image: require('../../assets/images/SolarRoof.jpeg'),
+  },
+];
